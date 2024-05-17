94bee1a37c94bd0903e501b3b4da458d7217f030
min <rmdwjd76@naver.com>
2024-05-18 00:14:43 +0900

    관광지 리스트 불러오기

--- src/components/Map/TouristSpotList.vue
+++ src/components/Map/TouristSpotList.vue
@@ -1,40 +1,41 @@
 <template>
-    <div>
-      <div class="result" v-for="comment in comments" :key="comment.id">
-        <div>{{ comment.numData }}</div>
-        <div>{{ comment.stringData }}</div>
-      </div>
-      <InfiniteLoading @infinite="load"></InfiniteLoading>
+  <div>
+    <div class="result" v-for="comment in comments" :key="comment.touristspotId">
+      <div>{{ comment.touristspotTitle }}</div>
+      <div>{{ comment.touristspotAddr }}</div>
     </div>
-  </template>
-  
-  <script setup>
-  import { ref } from 'vue';
-  import InfiniteLoading from 'v3-infinite-loading';
-  
-  let comments = ref([]);
-  let page = 1;
-  
-  const load = async $state => {
-    try {
-      const response = await fetch(`http://localhost:8080/touristspot`,{
-        params:{
-            pageSize : 10,
-            pageNum : 1
-        }
-      });
-      const json = await response.json();
-      if (json.length < 10) {
-        $state.complete();
-      } else {
-        comments.value.push(...json);
-        $state.loaded();
+    <InfiniteLoading @infinite="load"></InfiniteLoading>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import axios from 'axios';
+import InfiniteLoading from 'v3-infinite-loading';
+
+let comments = ref([]);
+let page = ref(1);
+
+const load = async $state => {
+  try {
+    const response = await axios.get('http://localhost:8080/touristspot', {
+      params: {
+        pageSize: 10,
+        pageNum: page.value
       }
-      page++;
-    } catch (error) {
-      $state.error();
-      console.error('Error loading more items:', error);
+    });
+    const json = response.data;
+    console.log(json);
+    if (json.length < 10) {
+      $state.complete();
+    } else {
+      comments.value.push(...json);
+      $state.loaded();
     }
-  };
-  </script>
-  
+    page.value++;
+  } catch (error) {
+    $state.error();
+    console.error('Error loading more items:', error);
+  }
+};
+</script>
