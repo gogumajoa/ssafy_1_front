88441aac4a194d34b49c4f2d4ea1518bcf6b6ccb
min <rmdwjd76@naver.com>
2024-05-19 16:11:54 +0900

    update list

--- src/components/Map/TouristSpotList.vue
+++ src/components/Map/TouristSpotList.vue
@@ -1,14 +1,12 @@
 <template>
   <div class="results-container">
-    
-      <div class="result">
-        <div>
-          <h5>{{ spot.touristspotTitle }}</h5> 
-          <p>{{ spot.description }}</p>
-        </div>
-        <div>{{ spot.touristspotAddr }}</div>
+    <div v-for="spot in touristSpots" :key="spot.id" class="result">
+      <div>
+        <h5>{{ spot.touristspotTitle }}</h5>
+        <p>{{ spot.description }}</p>
       </div>
-   
+      <div>{{ spot.touristspotAddr }}</div>
+    </div>
     <InfiniteLoading @infinite="load"></InfiniteLoading>
   </div>
 </template>
@@ -19,13 +17,14 @@ import axios from 'axios';
 import InfiniteLoading from 'v3-infinite-loading';
 
 const touristSpots = ref([]);
-
+let page=1;
 const load = async $state => {
   try {
     const response = await axios.get('http://localhost:8080/touristspot', {
       params: {
         pageSize: 10,
-        pageNum: touristSpots.value.length / 10 + 1
+        pageNum: page*10+1,
+        //touristSpots.value.length / 10 + 1
       }
     });
     const json = response.data;
@@ -33,16 +32,24 @@ const load = async $state => {
       $state.complete();
     } else {
       touristSpots.value.push(...json);
+      $state.loaded(); // Notify InfiniteLoading that more items have been loaded
     }
-    
+    page++;
+    console.log(touristSpots.value);
 
   } catch (error) {
     console.error('Error loading more items:', error);
+    $state.complete(); // Ensure the loading state is reset in case of error
   }
 };
 
 // 페이지 로드시 초기 데이터 불러오기
-onMounted(load);
+onMounted(() => {
+  load({
+    complete() {},
+    loaded() {}
+  });
+});
 </script>
 
 <style>
@@ -57,7 +64,7 @@ onMounted(load);
   z-index: 10;
   border-radius: 10px;
   left: 20px;
-  background-color:white;
+  background-color: white;
 }
 .result {
   padding: 10px;
